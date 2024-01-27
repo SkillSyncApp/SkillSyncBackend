303584b8d10d715bd776ccfa3246fb81618eb942
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-01-27 19:25:15 +0200

    PostController: create post override + declare Request type

--- src/middlewares/auth_middleware.ts
+++ src/middlewares/auth_middleware.ts
@@ -1,10 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export interface AuthResquest extends Request {
-    user?: { _id: string };
-}
-const authMiddleware = (req: AuthResquest, res: Response, next: NextFunction) => {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
     
