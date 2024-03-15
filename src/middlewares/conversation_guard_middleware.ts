49fe8a6580970c1481220d712dae03e5a53f2c6a
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-03-15 20:30:57 +0200

    use guard on sockets too

--- src/middlewares/conversation_guard_middleware.ts
+++ src/middlewares/conversation_guard_middleware.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import ChatModel from '../models/chat';
 
+export const isUserPartOfConversation = async (conversationId: string, userId: string) => {
+    return (await ChatModel.find({
+        users: { $in: [userId] },
+        _id: conversationId,
+    })).length > 0;
+}
+
 const conversationGuardMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const { id: conversationId } = req.params;
     const userId = req.user._id;
 
-    const isUserPartOfConversation = (await ChatModel.find({
-        users: { $in: [userId] },
-        _id: conversationId,
-    })).length > 0;
+    const canAccessConversation = await isUserPartOfConversation(conversationId, userId);
 
-    if (!isUserPartOfConversation) {
+    if (!canAccessConversation) {
         return res.status(403).send('User is not part of conversation');
     }
-    
+
     next();
 }
 
