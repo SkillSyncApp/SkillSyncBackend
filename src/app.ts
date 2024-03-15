4d1e58c9a2f9017e31e23174e6d427f3c38c91bc
hadar zaguri <hadarza@wix.com>
2024-03-15 18:56:07 +0200

    more tests + limit 10MB on express.json()

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const initApp = async (): Promise<Express> => {
      ***********************************************************************************/
 
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json({ limit: '10mb' }));
     app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
     // Add APIs
--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,6 +1,5 @@
 import request from "supertest";
-import app from "../app";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import { Express } from "express";
 import initApp from "../app";
 import User from "../models/user";
@@ -12,8 +11,16 @@ describe("User Endpoints", () => {
   let app: Express;
 
   const userData = {
-    name: "John Doe",
-    email: "john.doe@example.com",
+    name: "Nofar Shapir",
+    email: "nofarshapir00@example.com",
+    password: "password123",
+    type: "student",
+    bio: "Sample bio",
+  };
+
+  const userData2 = {
+    name: "Amit Brickman",
+    email: "amit.brickman@gmail.com",
     password: "password123",
     type: "student",
     bio: "Sample bio",
@@ -21,16 +28,26 @@ describe("User Endpoints", () => {
 
   beforeAll(async () => {
     app = await initApp();
-    await User.deleteMany({ email: userData.email });
+
+    await User.deleteMany({ email: { $in: [userData.email, userData2.email] } });
 
     const response = await request(app)
       .post("/api/auth/register")
       .send(userData);
     expect(response.statusCode).toBe(201);
 
+    const response2 = await request(app)
+      .post("/api/auth/register")
+      .send(userData2);
+    expect(response.statusCode).toBe(201);
+
     const authResponse = await request(app)
+    .post("/api/auth/login")
+    .send({ email: userData.email, password: userData.password });
+
+    await request(app)
       .post("/api/auth/login")
-      .send({ email: userData.email, password: userData.password });
+      .send({ email: userData2.email, password: userData2.password });
 
     accessToken = authResponse.body.accessToken;
     userId = authResponse.body.user._id;
@@ -52,6 +69,20 @@ describe("User Endpoints", () => {
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty("name", userData.name);
       expect(response.body).toHaveProperty("email", userData.email);
+    });
+  });
+
+  test("should get all user details", async () => {
+    const response = await request(app)
+      .get(`/api/users`)
+      .set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+    
+    response.body.forEach(user => {
+      expect(user).toHaveProperty("name");
+      expect(user).toHaveProperty("email");
     });
   });
 });
